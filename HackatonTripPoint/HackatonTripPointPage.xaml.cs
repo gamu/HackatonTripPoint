3c824f66cdb257adf57ff4c2a99e7315e9651b23
Иван Гришин <gamu@MBP-Ivan.sk.lan>
2017-03-16 11:12:56 +0300

    first commit

---
+++ HackatonTripPoint/HackatonTripPointPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace HackatonTripPoint
+{
+    public partial class HackatonTripPointPage : ContentPage
+    {
+        public HackatonTripPointPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
